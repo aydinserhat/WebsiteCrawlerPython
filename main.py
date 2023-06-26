29c8d9cf49630ba07053b8febd79c06f582ae780
Aydın Serhat Seyitoğlu <aydinserhatseyitoglu@gmail.com>
2023-06-26 15:55:42 +0300

    added control block

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 target_url = ""
+foundLinks = []
+def make_requests(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+def crawl(url):
+    links = make_requests(url)
+    for link in links.find_all('a'):
+        found_link = link.get('href')
+        if found_link is not None:# if found_links is not none - found_links non değilse
+            if "#" in found_link:
+                found_link = found_link.split('#')[0]
+            if target_url in found_link and found_link not in foundLinks:
+                foundLinks.append(found_link)
+                print(found_link)
+                crawl(found_link)#recursive
 
-response = requests.get(target_url)
-
-soup = BeautifulSoup(response.text)
-
-for link in soup.find_all('a'):
-    print(link.get('href'))
+crawl(target_url)
